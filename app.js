db9b192ff2a7e2b8f40d7fc7945c183d35eccc66
cflo <cflo013@gmail.com>
2020-03-20 10:24:03 +0800

    Add routes and views for register and login pages

--- app.js
+++ app.js
@@ -12,8 +12,31 @@ app.use(methodOverride('_method'))
 
 // 設定路由
 // 苜頁
+// 設定路由
+// 首頁
 app.get('/', (req, res) => {
-  res.send('hello todo sequelize project')
+  res.send('hello new todo-sequelize project')
+})
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 
