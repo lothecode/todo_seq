112799417030381cefd545247b295f89ec9b18dd
cflo <cflo013@gmail.com>
2020-03-23 09:48:09 +0800

    Add warninig message in login page

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,25 @@
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
+
 //載入User model
 const db = require('../models')
 const User = db.User
 
 module.exports = passport => {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      let errors = []
+    // 加入 passReqToCallback: true 在後面的 callback 中可以使用 req 參數
+    new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
       User.findOne({ where: { email: email } })
         .then(user => {
           if (!user) {
-            return done(null, false, { message: 'This email is not registered' })
+            return done(null, false, req.flash('warning_msg', '輸入的email未註冊'))
           }
           bcrypt.compare(password, user.password, (err, isMatch) => {
             if (err) throw err
             if (isMatch) {
               return done(null, user)
             } else {
-              return done(null, false, { messasage: 'email or password incorrect' })
+              return done(null, false, req.flash('warning_msg', 'Email or Password 錯誤!'))
             }
           })
 
